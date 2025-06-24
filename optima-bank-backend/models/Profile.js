2c336db5e321a28caa8ade5d79214ac50259ac19
ariezaaziera <ariezaziera@gmail.com>
2025-06-24 20:38:31 +0800

    Fixed edit profile issue

--- optima-bank-backend/models/Profile.js
+++ optima-bank-backend/models/Profile.js
@@ -3,21 +3,23 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   firstName: String,
   lastName: String,
-  username: String,
-  email: String,
-  phone: String,
-  password: String,
-  gender: String,
-  language: String,
-  address: String,
-  postcode: String,
-  country: String,
-  payment: String,
   dob: Date,
-  profileImage: String, // <-- URL gambar profil
-  provider: String,
+  phone: String,
+  username: String,
+  email: { type: String, required: true, unique: true, lowercase: true, },
+  password: {
+    type: String,
+    required: function () {
+      return this.provider === 'local'; // ✅ Only required for email/password users
+    }
+  },
+  provider: {
+    type: String,
+    default: 'local' // 'google' or 'local'
+  },
   resetToken: String,
   tokenExpiry: Date,
+  profileImage: String,
 });
 
-module.exports = mongoose.models.User || mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema);
