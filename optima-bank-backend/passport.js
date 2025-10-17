cc2bdc9bf4a3277fedd2e5e6581356e2d32292cd
Arieza Aziera <152407762+ariezaziera@users.noreply.github.com>
2025-10-18 02:43:53 +0800

    Update passport.js

--- optima-bank-backend/passport.js
+++ optima-bank-backend/passport.js
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,      // ✅ make sure these are in your .env
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/auth/google/callback",
+      callbackURL: "https://optimabank-gift1.onrender.com/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
